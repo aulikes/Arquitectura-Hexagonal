ef289ec07dda1d56c8ff89439b20d1dbc1b0558d
Mike <aulikes@hotmail.com>
2024-07-14 03:29:28 -0500

    ArqHexMIO init

---
+++ ArquitecturaHexagonalMIO/src/main/java/com/aug/hexagonal/ArquitecturaHexagonalMIO/ArquitecturaHexagonalMioApplication.java
@@ -0,0 +1,13 @@
+package com.aug.hexagonal.ArquitecturaHexagonalMIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaHexagonalMioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaHexagonalMioApplication.class, args);
+	}
+
+}
---
+++ ArquitecturaHexagonalMIO/src/test/java/com/aug/hexagonal/ArquitecturaHexagonalMIO/ArquitecturaHexagonalMioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aug.hexagonal.ArquitecturaHexagonalMIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaHexagonalMioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
